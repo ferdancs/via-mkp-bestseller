a434531f4ca909b420685c763d8ec044f9a4ca12
ferdancs <71042619+ferdancs@users.noreply.github.com>
2021-05-30 21:58:52 -0300

    Add files via upload

---
+++ CSV_TO_JSON.py
@@ -0,0 +1,10 @@
+import csv
+import json
+
+csvfile = open('C:/Users/2104624262/Downloads/volume_vendas_b5.csv', 'r', encoding='UTF-8')
+jsonfile = open('C:/Users/2104624262/Downloads/volume_vendas_b5.json', 'w')
+
+fieldnames = ('Pub','idskureferencia','idlojista','estado','nomedepartamento','nomesetor','qtd')
+reader = csv.DictReader(csvfile, fieldnames)
+out = json.dumps( [row for row in reader ] )
+jsonfile.write(out)
