e4c682c41aea699183fe8ecfb97b5099059f214f
ferdancs <71042619+ferdancs@users.noreply.github.com>
2021-05-30 22:00:59 -0300

    Update CSV_TO_JSON.py

--- CSV_TO_JSON.py
+++ CSV_TO_JSON.py
@@ -1,7 +1,7 @@
 import csv
 import json
 
-# Alterar os nomes dos aquivos finais dos notebook's (os aquivos csv's)!
+# Alterar os nomes dos aquivos finais dos notebook's (os aquivos csv's) e rodar o programa!
 csvfile = open('C:/Users/2104624262/Downloads/volume_vendas_b5.csv', 'r', encoding='UTF-8')
 jsonfile = open('C:/Users/2104624262/Downloads/volume_vendas_b5.json', 'w')
 
